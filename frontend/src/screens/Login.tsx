1ad7bdaa07deb1a42b4995ffa0ef46884e703e3b
leeyedam <dldpeka0909@gmail.com>
2023-05-29 17:43:07 +0900

    design: 로고추가 및 스타일 수정

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -15,6 +15,9 @@ const Login = () => {
 
   return (
     <View style={styles.rootContainer}>
+      <View style={styles.logoContainer}>
+        <Text style={styles.logo}>Date Plan</Text>
+      </View> 
       <View style={styles.inputContainer}> 
         <Text style={styles.label}>전화번호</Text>
         <Controller
@@ -34,7 +37,6 @@ const Login = () => {
         name="phoneNumber"
       />
       {errors.phoneNumber && <Text style={styles.errorMsg}>This is required.</Text>}
-
       <Text style={styles.label}>비밀번호</Text>
       <Controller
         control={control}
@@ -53,26 +55,29 @@ const Login = () => {
         name="password"
       />
       {errors.password && <Text style={styles.errorMsg}>This is required.</Text>}
-
-      <BouncyCheckbox
-      style={styles.checkbox}
-      size={18}
-      fillColor="black"
-      unfillColor="#FFFFFF"
-      text="자동 로그인"
-      textStyle={{
-        textDecorationLine: "none",
-        fontSize:14,
-        color:"#000000"
-      }}
-      iconStyle={{ borderColor: "black" }}
-      onPress={(isChecked: boolean) => {}}
-    />
+        <BouncyCheckbox
+        style={styles.checkbox}
+        size={18}
+        fillColor="black"
+        unfillColor="#FFFFFF"
+        text="자동 로그인"
+        textStyle={{
+          textDecorationLine: "none",
+          fontSize:14,
+          color:"#000000"
+        }}
+        iconStyle={{ borderColor: "black" }}
+        onPress={(isChecked: boolean) => {}}
+      />
     </View>
-    <Text 
-    style={styles.signupText}
-    onPress={() => Linking.openURL('http://google.com')}>회원가입하기</Text>
-    <ButtonComponent disabled={false} text='로그인' font='bold' onPress={handleSubmit(onSubmit)}/>
+    <View style={styles.signupContainer}>
+      <Text 
+      style={styles.signupText}
+      onPress={() => Linking.openURL('')}>회원가입하기</Text>
+      </View>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent disabled={false} text='로그인' font='bold' onPress={handleSubmit(onSubmit)}/>
+    </View>
   </View>
     
   );
@@ -82,8 +87,23 @@ export default Login;
 
 const styles = StyleSheet.create({
   rootContainer: {
+    flex: 1,
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  logoContainer:{
+    height:200,
+  },
+  logo:{
+    color: '#000000',
+    fontFamily:"Pretendard-Regular",
+    fontSize:30,
+    paddingTop: 76,
+  },
+  inputContainer: {
+    height:290,
     justifyContent: 'center',
+    marginTop:32,
   },
   label: {
     color: '#000000',
@@ -91,10 +111,6 @@ const styles = StyleSheet.create({
     fontSize:16,
     marginTop: 32,
     marginBottom:5
-  },
-  inputContainer: {
-    justifyContent: 'center',
-    marginTop:70,
   },
   input: {
     width: 327,
@@ -113,10 +129,17 @@ const styles = StyleSheet.create({
     color:"red",
     marginTop:2
   },
+  signupContainer:{
+    height:40
+  },
 signupText:{
   color: "#909090",
   textDecorationLine:'underline',
+  fontSize:14,
   marginBottom:23
+},
+buttonContainer:{
+  height:106
 }
 });
 
