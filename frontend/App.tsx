4708590da50caee4008d4e06344342853fc5c934
hyeone999 <caps026@naver.com>
2023-05-30 22:32:44 +0900

    feat: Navigation 기본 구조 생성

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import RegisterPhoneNum from './src/screens/RegisterPhoneNum';
 import RegisterUserInfo from './src/screens/RegisterUserInfo';
 import Login from './src/screens/Login';
 
+export enum RouteScreens {
+  RegisterPhone = 'RegisterPhone',
+  RegisterInfo = 'RegisterInfo',
+  LoginScreen = 'Login',
+}
+
 function App() {
+  const Stack = createStackNavigator();
+
   return (
     <RecoilRoot>
-      {/* <RegisterPhoneNum /> */}
-      <Login/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name={RouteScreens.RegisterPhone}
+            component={RegisterPhoneNum}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name={RouteScreens.RegisterInfo}
+            component={RegisterUserInfo}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name={RouteScreens.LoginScreen} component={Login} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </RecoilRoot>
   );
 }
