e8514f68d7979d444ff74720c14357c11aee2ed2
hyeone999 <caps026@naver.com>
2023-05-30 12:07:53 +0900

    feat: 휴대폰 인증 변경 UI 적용

--- frontend/src/screens/Register.tsx
+++ frontend/src/screens/Register.tsx
@@ -9,34 +9,31 @@ import {
   View,
 } from 'react-native';
 import React, { useState } from 'react';
-import DragIcon from '../assets/images/drag.svg';
-import Checkbox from '../components/design/CheckBoxComponent';
 import ButtonComponent from '../components/design/ButtonComponent';
 
 const Register = () => {
-  // 와이어프레임 임시 디자인 => 휴대폰만 인증하기로함
-
+  // 폰번호, 코드 data type 확인하기
   const [phoneNumber, setPhoneNumber] = useState('');
   const [codeNumber, setCodeNumber] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={{ flex: 1 }}>
         <SafeAreaView style={styles.container}>
+          {/* 헤더 UI */}
           <Text style={styles.headFont}>
             회원가입을 위해{'\n'}
             휴대폰 번호를 인증해주세요
           </Text>
-          <View style={styles.boxView}>
-            <Checkbox />
-            <Text style={styles.checkFont}>본인 확인을 위한 약관 모두 동의</Text>
-            <DragIcon />
-          </View>
-          <View style={{ marginTop: 25 }}>
-            <Text>휴대폰 인증</Text>
+          <Text style={styles.subFont}>본인인증을 하시면 휴대폰 번호로{'\n'}로그인을 할 수 있어요</Text>
+
+          {/* 인증 UI */}
+          <View style={styles.inputView}>
+            <Text>휴대폰 번호</Text>
             <View style={{ flexDirection: 'row', marginTop: 5 }}>
               <TextInput
-                style={{ ...styles.numberInput, flex: 9 }}
+                style={styles.numberInput}
                 placeholder="휴대폰 11자리"
                 keyboardType="phone-pad"
                 placeholderTextColor="#909090"
@@ -44,12 +41,15 @@ const Register = () => {
                 onChangeText={(text) => setPhoneNumber(text)}
               />
               <TouchableOpacity activeOpacity={1.0} style={styles.sendButton}>
+                {/* API 구현할때 인증요청 - 재요청 텍스트 분기처리 */}
                 <Text style={styles.sendFont}>인증요청</Text>
               </TouchableOpacity>
             </View>
-            <View style={{ marginTop: 10 }}>
+            <View style={{ marginTop: 15 }}>
+              {/* 인증번호 정규식 불일치 -> border red, 3분 타이머 */}
+              <Text>인증번호</Text>
               <TextInput
-                style={styles.numberInput}
+                style={{ ...styles.numberInput, marginTop: 5, width: '100%' }}
                 placeholder="인증번호 6자리"
                 placeholderTextColor="#909090"
                 value={codeNumber}
@@ -58,8 +58,10 @@ const Register = () => {
             </View>
           </View>
         </SafeAreaView>
+
+        {/* 하단 버튼 UI */}
         <View style={styles.buttonView}>
-          <ButtonComponent disabled={false} text="인증완료" font="bold" onPress={() => console.log('인증완료')} />
+          <ButtonComponent disabled={disabled} text="인증완료" font="bold" onPress={() => console.log('인증완료')} />
         </View>
       </View>
     </TouchableWithoutFeedback>
@@ -85,6 +87,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 54,
   },
+  inputView: {
+    marginTop: 35,
+  },
   buttonView: {
     marginBottom: 50,
     marginLeft: 25,
@@ -98,11 +103,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#000000',
   },
-  checkFont: {
+  subFont: {
     fontFamily: 'Pretendard-Medium',
-    fontSize: 14,
     color: '#000000',
-    marginRight: 10,
+    fontSize: 16,
+    lineHeight: 20,
+    marginTop: 15,
   },
   sendFont: {
     fontSize: 12,
@@ -110,7 +116,7 @@ const styles = StyleSheet.create({
     color: '#000000',
   },
   numberInput: {
-    width: '100%',
+    width: '75%',
     color: '#000000',
     height: 44,
     borderWidth: 1,
@@ -121,11 +127,10 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   sendButton: {
-    width: '23%',
+    width: '22%',
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: '#CCCCCC',
-    borderWidth: 1,
+    backgroundColor: '#CCCCCC',
     borderRadius: 4,
   },
 });
