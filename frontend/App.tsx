f339c10c1b2ef31a5cfb7d210ae4217173d72f49
hyeone999 <caps026@naver.com>
2023-05-30 22:50:38 +0900

    feat: Navigation 파일 분리하여 관리

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,44 +1,11 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import RegisterPhoneNum from './src/screens/RegisterPhoneNum';
-import RegisterUserInfo from './src/screens/RegisterUserInfo';
-import Login from './src/screens/Login';
-import { RouteScreens, StackParamList } from './src/types/navigationType';
-
-const Stack = createStackNavigator<StackParamList>();
-
-const navTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: 'white',
-  },
-};
+import MainRoute from './src/routes/MainRoute';
 
 function App() {
   return (
     <RecoilRoot>
-      <NavigationContainer theme={navTheme}>
-        <Stack.Navigator>
-          <Stack.Screen
-            name={RouteScreens.RegisterPhoneScreen}
-            component={RegisterPhoneNum}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name={RouteScreens.RegisterInfoScreen}
-            component={RegisterUserInfo}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name={RouteScreens.LoginScreen}
-            component={Login}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <MainRoute />
     </RecoilRoot>
   );
 }
