c531572855c4e059ad958bba591de61f39bb1454
hyeone999 <caps026@naver.com>
2023-05-30 22:40:51 +0900

    feat: Navigation 타입 생성

---
+++ frontend/src/types/navigationType.ts
@@ -0,0 +1,12 @@
+export enum RouteScreens {
+  RegisterPhoneScreen = 'RegisterPhoneScreen',
+  RegisterInfoScreen = 'RegisterInfoScreen',
+  LoginScreen = 'LoginScreen',
+}
+
+// 필요한 파라미터가 없는 상태
+export type StackParamList = {
+  RegisterPhoneScreen: undefined;
+  RegisterInfoScreen: undefined;
+  LoginScreen: undefined;
+};
