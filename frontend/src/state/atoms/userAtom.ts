5c4e8706bb7f708f4304f0da2ce4cbb91be59813
hyeone999 <caps026@naver.com>
2023-06-01 21:36:28 +0900

    fix: render err fix

--- frontend/src/state/atoms/userAtom.ts
+++ frontend/src/state/atoms/userAtom.ts
@@ -1,9 +1,8 @@
 import { atom } from 'recoil';
-import { v4 as uuidv4 } from 'uuid';
 import { userProps } from '../../types/userStateType';
 
 export const userState = atom<userProps>({
-  key: `userAtom/${uuidv4()}`,
+  key: `userAtom`,
   default: {
     // 유저 정보 이어서 추가 ex) 이름 ?
     phone: '',
