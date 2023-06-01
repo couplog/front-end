c644654ab6f3e970e55000c9da98c2b75324d5b7
hyeone999 <caps026@naver.com>
2023-06-01 21:09:03 +0900

    fix: Duplicate atom key' :

--- frontend/src/state/atoms/userAtom.ts
+++ frontend/src/state/atoms/userAtom.ts
@@ -1,9 +1,11 @@
 import { atom } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
 import { userProps } from '../../types/userStateType';
 
 export const userState = atom<userProps>({
-  key: 'userAtom',
+  key: `userAtom/${uuidv4()}`,
   default: {
+    // 유저 정보 이어서 추가 ex) 이름 ?
     phone: '',
   },
 });
