0ebdab11f3cb8f4e88381b29c07e5860a9e32637
hyeone999 <caps026@naver.com>
2023-06-03 13:42:11 +0900

    feat: 유저 정보 추가

--- frontend/src/state/atoms/userAtom.ts
+++ frontend/src/state/atoms/userAtom.ts
@@ -5,6 +5,11 @@ export const userState = atom<userProps>({
   key: `userAtom:${Math.random()}`,
   default: {
     // 유저 정보 이어서 추가 ex) 이름 ?
+    name: '',
+    nickname: '',
     phone: '',
+    birth: '',
+    gender: '',
+    password: '', // 패스워드는 민감 정보이기 때문에 추후 다시 구현 예정
   },
 });
