8a52c8ce7bdc8571b915b5fa7058f22fe41c20c3
hyeone999 <caps026@naver.com>
2023-06-03 12:30:45 +0900

    feat: Checkbox props, style 추가

--- frontend/src/components/design/CheckBoxComponent.tsx
+++ frontend/src/components/design/CheckBoxComponent.tsx
@@ -1,40 +1,53 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
 
-const Checkbox = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckboxToggle = () => {
-    setIsChecked(!isChecked);
-  };
-
+interface CheckBox {
+  label: string;
+  checked: boolean;
+  onPress: () => void;
+}
+const Checkbox = ({ checked, onPress, label }: CheckBox) => {
   return (
-    <TouchableOpacity onPress={handleCheckboxToggle}>
-      <View
-        style={{
-          width: 24,
-          height: 24,
-          borderRadius: 12,
-          borderWidth: 2,
-          borderColor: '#EDF0F3',
-          backgroundColor: '#FFFFFF',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {isChecked && (
-          <View
-            style={{
-              width: '80%',
-              height: '80%',
-              borderRadius: 12,
-              backgroundColor: '#000000',
-            }}
-          />
-        )}
+    <TouchableOpacity
+      activeOpacity={1.0}
+      onPress={onPress}
+      style={styles.buttonContainer}
+    >
+      <View style={styles.checkboxContainer}>
+        {checked && <View style={styles.onCheck} />}
       </View>
+      <Text style={styles.titleText}>{label}</Text>
     </TouchableOpacity>
   );
 };
 
 export default Checkbox;
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    width: 20,
+    height: 20,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: '#EDF0F3',
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  onCheck: {
+    width: '75%',
+    height: '75%',
+    borderRadius: 12,
+    backgroundColor: '#000000',
+  },
+  titleText: {
+    color: '#000000',
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 14,
+    marginLeft: 10,
+  },
+});
