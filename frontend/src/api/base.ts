9afa8859a5970f79bd8489d85d8575cf3bba0f81
hyeone999 <caps026@naver.com>
2023-06-06 20:47:49 +0900

    feat: 임시 api base 설정

---
+++ frontend/src/api/base.ts
@@ -0,0 +1,36 @@
+// 임시 백엔드 주소
+// 추후 .env 처리
+
+import axios from 'axios';
+
+const API = 'http://43.202.0.203';
+
+const request = axios.create({
+  baseURL: API,
+  headers: {
+    'Content-Type': 'application/json',
+    withCredentials: true,
+  },
+});
+
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+request.interceptors.response.use(
+  (response) => {
+    const res = response;
+    return res;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default request;
