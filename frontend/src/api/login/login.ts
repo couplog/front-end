d0ea300e293dd84d3b5e517082bbdb2d69e6790f
hyeone999 <caps026@naver.com>
2023-06-07 21:39:13 +0900

    comment: console 주석 추가(추후 삭제 예정)

--- frontend/src/api/login/login.ts
+++ frontend/src/api/login/login.ts
@@ -2,7 +2,8 @@ import { userProps } from '../../types/userStateType';
 import request from '../base';
 
 export const handleLogin = (userInfo: userProps) => {
-  console.log('userInfo: ', userInfo);
+  // console.log('userInfo: ', userInfo); 추후 삭제 예정
+
   return request({
     method: 'POST',
     url: '/api/auth/signup',
