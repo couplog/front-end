a18904ed7e55b3b9ee08df91473788b81706bb7a
leeyedam <dldpeka0909@gmail.com>
2023-06-08 21:00:40 +0900

    design: 패스워드 인풋 눈 아이콘 추가

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,9 +1,12 @@
 import {
+  Keyboard,
   Linking,
   SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import React, { useState } from 'react';
@@ -11,6 +14,8 @@ import { Controller, useForm } from 'react-hook-form';
 import { StackScreenProps } from '@react-navigation/stack';
 import { StackParamList } from '../types/navigationType';
 import ButtonComponent from '../components/design/ButtonComponent';
+import OffEye from '../assets/images/register/off_eye.svg';
+import OnEye from '../assets/images/register/on_eye.svg';
 import Checkbox from '../components/design/CheckBoxComponent';
 import { LoginFormData } from '../types/loginFormType';
 import { handleLogin } from '../api/login/login';
@@ -38,75 +43,90 @@ const Login = ({ navigation }: Props) => {
     }
   };
   const [checked, setChecked] = useState(false);
+  const [eyeClick, setEyeClick] = useState(false);
 
   return (
-    <SafeAreaView style={styles.rootContainer}>
-      <View style={styles.logoView}>
-        <Text style={styles.logo}>Date Plan</Text>
-      </View>
-      <View style={styles.inputView}>
-        <Text style={styles.label}>전화번호</Text>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.input}
-              placeholder="전화번호를 입력해주세요"
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.rootContainer}>
+        <View style={styles.logoView}>
+          <Text style={styles.logo}>Date Plan</Text>
+        </View>
+        <View style={styles.inputView}>
+          <Text style={styles.label}>전화번호</Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                placeholder="전화번호를 입력해주세요"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="phone"
+          />
+          {errors.phone && (
+            <Text style={styles.errorFont}>This is required.</Text>
           )}
-          name="phone"
-        />
-        {errors.phone && (
-          <Text style={styles.errorFont}>This is required.</Text>
-        )}
-        <Text style={styles.label}>비밀번호</Text>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.input}
-              placeholder="비밀번호를 입력해주세요"
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
+          <Text style={styles.label}>비밀번호</Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                placeholder="비밀번호를 입력해주세요"
+                secureTextEntry={!eyeClick}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                autoCapitalize="none"
+              />
+            )}
+            name="password"
+          />
+          <TouchableOpacity
+            activeOpacity={1.0}
+            onPress={() => setEyeClick((prev) => !prev)}
+            style={{
+              ...styles.eyeIconView,
+              bottom: errors.password ? 33 : 0,
+            }}
+          >
+            {eyeClick ? <OnEye /> : <OffEye />}
+          </TouchableOpacity>
+          {errors.password && (
+            <Text style={styles.errorFont}>This is required.</Text>
           )}
-          name="password"
-        />
-        {errors.password && (
-          <Text style={styles.errorFont}>This is required.</Text>
-        )}
-        <View style={styles.checkboxView}>
-          <Checkbox
-            checked={checked}
-            onPress={() => setChecked(!checked)}
-            label="자동 로그인"
+          <View style={styles.checkboxView}>
+            <Checkbox
+              checked={checked}
+              onPress={() => setChecked(!checked)}
+              label="자동 로그인"
+            />
+          </View>
+        </View>
+        <View style={styles.signupView}>
+          <Text style={styles.signupText} onPress={() => Linking.openURL('')}>
+            회원가입하기
+          </Text>
+        </View>
+        <View style={styles.buttonView}>
+          <ButtonComponent
+            disabled={false}
+            text="로그인"
+            font="bold"
+            onPress={handleSubmit(onSubmit)}
           />
         </View>
-      </View>
-      <View style={styles.signupView}>
-        <Text style={styles.signupText} onPress={() => Linking.openURL('')}>
-          회원가입하기
-        </Text>
-      </View>
-      <View style={styles.buttonView}>
-        <ButtonComponent
-          disabled={false}
-          text="로그인"
-          font="bold"
-          onPress={handleSubmit(onSubmit)}
-        />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -155,6 +175,11 @@ const styles = StyleSheet.create({
   errorFont: {
     color: 'red',
     marginTop: 2,
+  },
+  eyeIconView: {
+    position: 'absolute',
+    right: 15,
+    top: 137,
   },
   checkboxView: {
     flexDirection: 'row',
