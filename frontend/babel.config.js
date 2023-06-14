3a27363eec64614634b88cfed7fba1ff7f495141
hyeone999 <caps026@naver.com>
2023-06-14 11:52:02 +0900

    fix: env 설정

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,14 +4,7 @@ module.exports = {
     [
       'module:react-native-dotenv',
       {
-        envName: ' APP_ENV ',
-        moduleName: ' @env ',
-        path: ' .env ',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
+        moduleName: 'react-native-dotenv',
       },
     ],
   ],
