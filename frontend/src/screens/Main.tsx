33915ef096da7ac1ad77328ca95def7e7418f66e
hyeone999 <caps026@naver.com>
2023-06-17 13:07:18 +0900

    faet: main 페이지 API 등록

--- frontend/src/screens/Main.tsx
+++ frontend/src/screens/Main.tsx
@@ -1,31 +1,89 @@
 import { ImageBackground, SafeAreaView, StyleSheet, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { handleCoupleInfo } from '../api/couple/coupleInfo';
+import { coupleState } from '../state/atoms/coupleAtom';
 import Header from '../components/main/Header';
 import Profile from '../components/main/Profile';
 import Footer from '../components/main/Footer';
+import { handlePartnerProfile } from '../api/couple/partnerProfile';
+
+// - 커플 배경 사진 등록 / 수정 / 삭제 / 조회
+// - 각자 프로필 사진 조회
+// - 만난지 몇일 되었는지 조회
+// - 다음 기념일 조회 - 이건 기념일 페이지 완성되면(기능 구현 완료되면 진행)
 
 const Main = () => {
+  const [coupleInfo, setCoupleInfo] = useRecoilState(coupleState);
+
+  // 메인화면 접근 -> 커플 정보 조회, 상대방 프로필 조회
+  useEffect(() => {
+    const fetchCoupleInfo = async () => {
+      try {
+        const coupleInfoResponse = await handleCoupleInfo();
+        const { data } = coupleInfoResponse.data;
+
+        const updatedCoupleInfo = {
+          coupleId: data.coupleId,
+          firstDate: data.firstDate,
+          partnerId: data.partnerId,
+        };
+
+        setCoupleInfo((prevCoupleInfo) => ({
+          ...prevCoupleInfo,
+          ...updatedCoupleInfo,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchPartnerProfile = async () => {
+      try {
+        const partnerProfileResponse = await handlePartnerProfile(
+          coupleInfo.partnerId
+        );
+        const partnerImage = partnerProfileResponse.data.data.profileImageURL;
+
+        setCoupleInfo((prevCoupleInfo) => ({
+          ...prevCoupleInfo,
+          partnerImageUrl: partnerImage,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCoupleInfo();
+    fetchPartnerProfile();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <View style={{ flex: 1 }}>
-      {/* 임시 배경화면 */}
-      <ImageBackground
-        source={require('../assets/images/main/background.png')}
-        style={{ width: '100%', height: '100%' }}
-      >
-        <SafeAreaView style={styles.container}>
-          <View style={styles.margin}>
-            {/* Header UI */}
-            <Header />
+    <View style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.margin}>
+          {/* Header UI */}
+          <Header />
 
-            {/* 유저 프로필 & 연애 day UI */}
-            {/* 임시 구현(이미지) */}
-            <Profile />
-          </View>
+          {/* 유저 프로필 & 연애 day UI */}
+          {/* 임시 구현(이미지) */}
+          <Profile
+            meetDate={coupleInfo.firstDate}
+            partnerImageUrl={coupleInfo.partnerImageUrl}
+          />
+          {/* 임시 배경화면 (추후 API로 가져와야함) */}
+          <ImageBackground
+            source={require('../assets/images/main/background.png')}
+            style={{ width: '100%', height: '80%' }}
+            imageStyle={{ borderRadius: 15 }}
+          />
+        </View>
 
-          {/* Footer UI */}
-          <Footer />
-        </SafeAreaView>
-      </ImageBackground>
+        {/* Footer UI */}
+        <Footer />
+      </SafeAreaView>
     </View>
   );
 };
@@ -35,7 +93,6 @@ export default Main;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
   },
   margin: {
     marginLeft: 25,
