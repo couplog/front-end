7010a8a166541b35ad5802b85a6a3aa547499606
hyeone999 <caps026@naver.com>
2023-06-17 13:44:37 +0900

    feat: design: footer UI 수정(피그마 반영)

--- frontend/src/components/main/Footer.tsx
+++ frontend/src/components/main/Footer.tsx
@@ -1,29 +1,15 @@
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { BlurView } from '@react-native-community/blur';
 import Calendar from '../../assets/images/main/calendar.svg';
 
 const Footer = () => {
   return (
     <View style={styles.footerView}>
-      <View
-        style={{
-          ...styles.dDayBox,
-          backgroundColor: Platform.OS === 'android' ? '#d5d4d463' : undefined,
-        }}
-      >
-        {Platform.OS === 'ios' && (
-          <BlurView
-            blurType="light"
-            style={styles.absolute}
-            blurAmount={5}
-            reducedTransparencyFallbackColor="white"
-          />
-        )}
+      <View style={styles.dDayBox}>
         <View style={styles.itemFlex}>
           {/* 임시 사각 박스 */}
           <View style={{ width: 60, height: 60, backgroundColor: '#C8C8C8' }} />
-          <View>
+          <View style={{ gap: 3 }}>
             <Text style={styles.weText}>다음 (기념일)까지</Text>
             <Text style={styles.dayText}>83일 남았어요</Text>
           </View>
@@ -60,24 +46,16 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     gap: 10,
   },
-  absolute: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    borderRadius: 14,
-  },
   iconCalendar: { marginBottom: 35, marginRight: 10 },
   weText: {
     fontFamily: 'Pretendard-Regular',
     fontWeight: '600',
-    color: '#FFFFFF',
+    color: '#000000',
   },
   dayText: {
     fontSize: 20,
     fontFamily: 'Pretendard-Medium',
     fontWeight: '700',
-    color: '#FFFFFF',
+    color: '#000000',
   },
 });
