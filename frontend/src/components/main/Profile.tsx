632e7c6be29d98ec88cba0210999c32faea7956d
hyeone999 <caps026@naver.com>
2023-06-17 13:06:44 +0900

    feat: main 페이지 커플 프로필 기능 구현

--- frontend/src/components/main/Profile.tsx
+++ frontend/src/components/main/Profile.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../state/atoms/userAtom';
 import Heart from '../../assets/images/main/heart.svg';
+import { ProfileComponentProps } from '../../types/main/profileType';
 
-const Profile = () => {
+const Profile = ({ meetDate, partnerImageUrl }: ProfileComponentProps) => {
+  const userInfo = useRecoilValue(userState);
+  // 커플 만날 날짜 계산
+  const today = new Date();
+  const firstDate = meetDate;
+
+  const getDayDifference = (firstDate: string, secondDate: string): number => {
+    const date1 = new Date(firstDate);
+    const date2 = new Date(secondDate);
+    const oneDay = 24 * 60 * 60 * 1000;
+    const diffDays = Math.round(
+      Math.abs((date1.getTime() - date2.getTime()) / oneDay)
+    );
+    return diffDays;
+  };
+
+  const daysDifference = getDayDifference(today.toISOString(), firstDate);
+
   return (
     <>
       <View style={styles.coupleView}>
-        <ImageBackground
-          source={require('../../assets/images/main/boy.png')}
-          style={{ width: 50, height: 50 }}
-        />
-        <Heart />
-        <ImageBackground
-          source={require('../../assets/images/main/girl.png')}
-          style={{ width: 50, height: 50 }}
-        />
-      </View>
-      <View style={styles.dayView}>
-        <Text style={styles.weText}>우리가 만난지</Text>
-        <Text style={styles.dayText}>128일 째</Text>
+        <View style={styles.dayView}>
+          <Text style={styles.weText}>우리가 만난지</Text>
+          <Text style={styles.dayText}>{daysDifference}일 째</Text>
+        </View>
+        <View style={styles.profileView}>
+          {/* 본인 */}
+          <Image
+            source={{ uri: userInfo.profileImageUrl }}
+            style={{ width: 50, height: 50 }}
+          />
+          <Heart />
+          {/* 상대방  */}
+          <Image
+            source={{ uri: partnerImageUrl }}
+            style={{ width: 50, height: 50 }}
+          />
+        </View>
       </View>
     </>
   );
@@ -30,24 +54,30 @@ const styles = StyleSheet.create({
   coupleView: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     marginTop: 55,
+    marginBottom: 20,
+  },
+  profileView: {
+    flexDirection: 'row',
+    alignItems: 'center',
     gap: 5,
   },
   dayView: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20,
+    gap: 3,
   },
   weText: {
     fontFamily: 'Pretendard-Regular',
+    fontSize: 16,
     fontWeight: '600',
-    color: '#FFFFFF',
+    color: '#000000',
   },
   dayText: {
-    fontSize: 20,
+    fontSize: 24,
     fontFamily: 'Pretendard-Medium',
     fontWeight: '700',
-    color: '#FFFFFF',
+    color: '#000000',
   },
 });
