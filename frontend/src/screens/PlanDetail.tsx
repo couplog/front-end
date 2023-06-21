fa873e979c8a11c457d6118505e5dfaffb2f67e9
hyeone999 <caps026@naver.com>
2023-06-21 12:22:07 +0900

    feat: 일정 추가하기 페이지 생성

---
+++ frontend/src/screens/PlanDetail.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const PlanDetail = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>PlanDetail</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default PlanDetail;
+
+const styles = StyleSheet.create({});
