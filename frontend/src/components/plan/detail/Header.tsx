7596a5653ba96720a0cdd1c7df30388cdeb113d2
hyeone999 <caps026@naver.com>
2023-06-27 21:48:03 +0900

    feat: 수정된 피그마 반영된 PlanDetail 페이지 Header 컴포넌트 생성

--- frontend/src/components/plan/detail/Header.tsx
+++ frontend/src/components/plan/detail/Header.tsx
@@ -1,34 +1,27 @@
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-import XButton from '../../../assets/images/plan/xbutton.svg';
+import { useNavigation } from '@react-navigation/native';
+import Back from '../../../assets/images/register/back.svg';
 
-const Header = () => {
+interface PlanDetailProps {
+  disabled: boolean;
+  onPress: () => void;
+}
+
+const Header = ({ onPress, disabled }: PlanDetailProps) => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <View style={styles.headerView}>
-        {/* 버튼 클릭시 다시 일정 화면으로 리다이렉션 */}
-        <XButton />
+        <Back onPress={() => navigation.goBack()} />
         <TouchableOpacity
           activeOpacity={1.0}
-          onPress={() => console.log('일정 추가 완료')}
+          disabled={disabled}
+          onPress={onPress}
         >
-          <Text style={styles.headerText}>추가</Text>
+          <Text style={styles.headerText}>다음</Text>
         </TouchableOpacity>
-      </View>
-
-      {/* 일정 input */}
-      <View style={styles.inputView}>
-        <TextInput
-          placeholder="일정 제목을 입력하세요"
-          placeholderTextColor="#909090"
-          style={{ fontSize: 24, fontFamily: 'Pretendard-Medium' }}
-        />
       </View>
     </View>
   );
@@ -44,17 +37,10 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: 'row',
   },
-  inputView: {
-    marginTop: 25,
-    fontSize: 24,
-    fontFamily: 'Pretendard-Medium',
-    color: '#000000',
-    alignItems: 'center',
-  },
   headerText: {
     fontSize: 16,
     fontWeight: '600',
-    fontFamily: 'Pretendard-Medium',
+    fontFamily: 'Pretendard-Bold',
     color: '#000000',
   },
 });
