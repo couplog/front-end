e16758dff9b522d7d17592a87dbe9a8f5d04bc64
hyeone999 <caps026@naver.com>
2023-06-28 09:39:58 +0900

    feat: 피그마 수정사항 반영

--- frontend/src/components/plan/detail/Header.tsx
+++ frontend/src/components/plan/detail/Header.tsx
@@ -2,25 +2,26 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Back from '../../../assets/images/register/back.svg';
+import { PlanDetailProps } from '../../../types/plan/planDetailTypes';
 
-interface PlanDetailProps {
-  disabled: boolean;
-  onPress: () => void;
-}
-
-const Header = ({ onPress, disabled }: PlanDetailProps) => {
+const Header = ({ none, text, onPress, disabled }: PlanDetailProps) => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <View style={styles.headerView}>
-        <Back onPress={() => navigation.goBack()} />
+      <View
+        style={{
+          ...styles.headerView,
+          justifyContent: none ? 'flex-end' : 'space-between',
+        }}
+      >
+        {none ? null : <Back onPress={() => navigation.goBack()} />}
         <TouchableOpacity
           activeOpacity={1.0}
           disabled={disabled}
           onPress={onPress}
         >
-          <Text style={styles.headerText}>다음</Text>
+          <Text style={styles.headerText}>{text}</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -34,7 +35,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   headerView: {
-    justifyContent: 'space-between',
     flexDirection: 'row',
   },
   headerText: {
