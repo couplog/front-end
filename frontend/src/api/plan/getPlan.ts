b7d75672a2d874a2dfb08df2df572f01e58571c2
leeyedam <dldpeka0909@gmail.com>
2023-07-05 23:07:26 +0900

    feat: 데이트 일정 조회 API 추가

--- frontend/src/api/plan/getPlan.ts
+++ frontend/src/api/plan/getPlan.ts
@@ -20,3 +20,17 @@ export const handleGetPlanDetail = async (
     url: `/api/members/${memberId}/schedules?year=${year}&month=${month}&day=${day}`,
   });
 };
+
+// 데이트 일정 날짜 조회
+export const handleGetDatePlan = async (coupleId: number | null) => {
+  return request({
+    url: `/api/couples/${coupleId}/dating/dates`,
+  });
+};
+
+// 데이트 일정 디테일 조회
+export const handleGetDatePlanDetail = async (coupleId: number | null) => {
+  return request({
+    url: `/api/couples/${coupleId}/dating`,
+  });
+};
