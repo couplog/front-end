34636bf488b18962b6280d48c4e91020b246bfe7
leeyedam <dldpeka0909@gmail.com>
2023-07-06 21:50:09 +0900

    feat: swipe 구현

--- frontend/src/components/plan/CalendarDetailBox.tsx
+++ frontend/src/components/plan/CalendarDetailBox.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Swipeable } from 'react-native-gesture-handler';
 import { ScheduleDetailType } from '../../types/atom/scheduleDetailType';
 
 interface Props {
@@ -9,6 +10,19 @@ interface Props {
 }
 
 const CalendarDetailBox = ({ scheduleDetail, boxColor, noSchedule }: Props) => {
+  const rightSwipeActions = () => {
+    return (
+      <View style={{ flexDirection: 'row', height: 44 }}>
+        <View style={styles.swipeLeftView}>
+          <Text style={styles.swipeLeftText}>수정</Text>
+        </View>
+        <View style={styles.swipeRightView}>
+          <Text style={styles.swipeRightText}>삭제</Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.containerView}>
       {noSchedule ? (
@@ -16,12 +30,20 @@ const CalendarDetailBox = ({ scheduleDetail, boxColor, noSchedule }: Props) => {
           <Text style={styles.noScheduleText}>일정이 없습니다.</Text>
         </View>
       ) : (
-        <View style={styles.detailBoxView}>
-          <View style={{ ...styles.colorView, backgroundColor: boxColor }} />
-          <View style={styles.detailTextContainerView}>
-            <Text style={styles.detailHeaderText}>{scheduleDetail?.title}</Text>
-            <Text style={styles.placeText}>{scheduleDetail?.location}</Text>
-          </View>
+        <View style={{ flex: 1 }}>
+          <Swipeable renderRightActions={rightSwipeActions}>
+            <View style={{ ...styles.detailBoxView }}>
+              <View
+                style={{ ...styles.colorView, backgroundColor: boxColor }}
+              />
+              <View style={styles.detailTextContainerView}>
+                <Text style={styles.detailHeaderText}>
+                  {scheduleDetail?.title}
+                </Text>
+                <Text style={styles.placeText}>{scheduleDetail?.location}</Text>
+              </View>
+            </View>
+          </Swipeable>
         </View>
       )}
     </View>
@@ -34,28 +56,24 @@ const styles = StyleSheet.create({
   containerView: {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
     marginTop: 12,
     marginRight: 5,
+    height: 44,
+    borderColor: '#EDF0F3',
+    borderWidth: 1,
+    borderRadius: 8,
   },
   noScheduleBoxView: {
     flex: 1,
     display: 'flex',
     flexDirection: 'row',
-    height: 44,
-    borderColor: '#EDF0F3',
-    borderWidth: 1,
-    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
   },
   detailBoxView: {
-    flex: 1,
     display: 'flex',
     flexDirection: 'row',
-    height: 44,
-    borderColor: '#EDF0F3',
-    borderWidth: 1,
-    borderRadius: 8,
     borderLeftWidth: 0,
     alignItems: 'center',
   },
@@ -67,9 +85,11 @@ const styles = StyleSheet.create({
   },
   detailTextContainerView: {
     flex: 1,
+    width: 327,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     paddingLeft: 12,
     paddingRight: 12,
   },
@@ -90,5 +110,31 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     fontSize: 14,
     color: '#CCCCCC',
+  },
+  swipeLeftView: {
+    width: 53,
+    backgroundColor: '#909090',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  swipeLeftText: {
+    fontFamily: 'Pretendard-Medium',
+    fontWeight: '400',
+    fontSize: 12,
+    color: '#FFFFFF',
+  },
+  swipeRightView: {
+    width: 53,
+    backgroundColor: '#FE3D2F',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+  swipeRightText: {
+    fontFamily: 'Pretendard-Medium',
+    fontWeight: '400',
+    fontSize: 12,
+    color: '#FFFFFF',
   },
 });
