c791f43b8814f475c77ad4b995125dc569f4aaf4
hyeone999 <caps026@naver.com>
2023-07-08 10:24:15 +0900

    feat: 캘린더 스타일 코드 분기처리

--- frontend/src/components/plan/MultipleCalendarBox.tsx
+++ frontend/src/components/plan/MultipleCalendarBox.tsx
@@ -1,19 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
 
-interface Props {
-  color?: string;
-}
+const MultipleCalendarBox = ({ color }: { color?: string }) => {
+  const calendarBoxStyle = {
+    flex: 1,
+    backgroundColor: color,
+  };
 
-const MultipleCalendarBox = ({ color }: Props) => {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          ...styles.calendarBoxView,
-          backgroundColor: color,
-        }}
-      />
+      <View style={calendarBoxStyle} />
     </View>
   );
 };
@@ -24,8 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-  },
-  calendarBoxView: {
-    flex: 1,
   },
 });
