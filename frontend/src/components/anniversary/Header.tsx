42d2c31fb3e5779a85314120479649af809fcdb7
hyeone999 <caps026@naver.com>
2023-07-08 22:41:52 +0900

    feat: 헤더 컴포넌트 props에 따른 분기처리

--- frontend/src/components/anniversary/Header.tsx
+++ frontend/src/components/anniversary/Header.tsx
@@ -1,28 +1,36 @@
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Plus from '../../assets/images/common/boldPlus.svg';
 import Back from '../../assets/images/common/smallBack.svg';
+import { HeaderProps } from '../../types/anniversary/types';
+import XButton from '../../assets/images/common/xButton.svg';
 
-const Header = ({ onPress }: { onPress: () => void }) => {
+const Header = ({ onPress, create, isDisabled }: HeaderProps) => {
   const navigation = useNavigation();
 
+  const containerStyle = {
+    ...styles.container,
+    marginBottom: create ? undefined : -50,
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={containerStyle}>
       <TouchableOpacity
         activeOpacity={1.0}
         hitSlop={styles.hitSlop}
         onPress={() => navigation.goBack()}
       >
-        <Back />
+        {create ? <XButton /> : <Back />}
       </TouchableOpacity>
 
       <TouchableOpacity
+        disabled={isDisabled}
         activeOpacity={1.0}
         hitSlop={styles.hitSlop}
         onPress={onPress}
       >
-        <Plus onPress={onPress} />
+        {create ? <Text style={styles.createText}>추가</Text> : <Plus />}
       </TouchableOpacity>
     </View>
   );
@@ -36,12 +44,17 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingTop: 25,
-    marginBottom: -50,
   },
   hitSlop: {
     top: 10,
     bottom: 10,
     left: 10,
     right: 10,
+  },
+  createText: {
+    fontSize: 16,
+    fontFamily: 'Pretendard-Medium',
+    fontWeight: '600',
+    color: '#000000',
   },
 });
