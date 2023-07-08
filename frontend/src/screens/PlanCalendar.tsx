9ed97baef045d0e561cead053a9858e598bb4176
hyeone999 <caps026@naver.com>
2023-07-08 10:23:00 +0900

    remove: 불필요한 스타일 코드 삭제

--- frontend/src/screens/PlanCalendar.tsx
+++ frontend/src/screens/PlanCalendar.tsx
@@ -2,13 +2,9 @@ import {
   Keyboard,
   SafeAreaView,
   StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
   TouchableWithoutFeedback,
-  View,
 } from 'react-native';
-import React, { useState } from 'react';
+import React from 'react';
 
 import { StackScreenProps } from '@react-navigation/stack';
 import { StackParamList } from '../types/routes/navigationType';
@@ -19,11 +15,9 @@ type Props = StackScreenProps<StackParamList, 'PlanCalendarScreen'>;
 const PlanCalendar = ({ navigation }: Props) => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.marginContainer}>
-        <SafeAreaView style={styles.rootContainer}>
-          <CheckCalendar />
-        </SafeAreaView>
-      </View>
+      <SafeAreaView style={styles.rootContainer}>
+        <CheckCalendar detail={false} />
+      </SafeAreaView>
     </TouchableWithoutFeedback>
   );
 };
@@ -31,74 +25,7 @@ const PlanCalendar = ({ navigation }: Props) => {
 export default PlanCalendar;
 
 const styles = StyleSheet.create({
-  marginContainer: {
-    flex: 1,
-  },
   rootContainer: {
     flex: 1,
-  },
-  logoView: {
-    width: 200,
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    backgroundColor: '#E7E7E7',
-    marginTop: 80,
-  },
-  logo: {
-    fontFamily: 'Pretendard-Bold',
-    color: '#909090',
-    fontSize: 30,
-  },
-  errorText: {
-    fontSize: 12,
-    lineHeight: 15,
-    color: '#E53C3C',
-    marginTop: 15,
-    fontFamily: 'Pretendard-Regular',
-  },
-  inputView: {
-    width: '100%',
-    height: 200,
-    justifyContent: 'center',
-    marginTop: 64,
-  },
-  label: {
-    color: '#000000',
-    fontFamily: 'Pretendard-Regular',
-    marginTop: 32,
-    marginBottom: 5,
-  },
-  inputBox: {
-    width: '100%',
-    backgroundColor: 'white',
-    borderWidth: 1,
-    borderColor: '#EDF0F3',
-    height: 45,
-    borderRadius: 8,
-    paddingLeft: 12,
-  },
-  passwordView: {
-    position: 'relative',
-  },
-  eyeIconView: {
-    position: 'absolute',
-    right: 15,
-    top: 12,
-  },
-  checkboxView: {
-    flexDirection: 'row',
-    marginTop: 10,
-  },
-  signupText: {
-    color: '#909090',
-    textDecorationLine: 'underline',
-    fontFamily: 'Pretendard-Regular',
-    marginBottom: 25,
-  },
-  buttonView: {
-    width: '100%',
-    marginBottom: 50,
   },
 });
