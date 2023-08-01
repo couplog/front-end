a366352bbb4f8ec76f3faed9cff3fd8e99830106
hyeone999 <caps026@naver.com>
2023-08-01 12:25:52 +0900

    feat: 일정 수정하기(시작, 종료시간) 구현

--- frontend/src/screens/PlanDetail/PlanEnd.tsx
+++ frontend/src/screens/PlanDetail/PlanEnd.tsx
@@ -1,24 +1,27 @@
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import { StackScreenProps } from '@react-navigation/stack';
-import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 import Header from '../../components/plan/detail/Header';
 import InputGroup from '../../components/plan/detail/InputGroup';
 import Footer from '../../components/plan/detail/Footer';
 import { StackParamList } from '../../types/routes/navigationType';
 import { planState } from '../../state/atoms/userPlanDetail';
+import { editModeState } from '../../state/atoms/createEditModeAtom';
 
 type Props = StackScreenProps<StackParamList, 'PlanEndScreen'>;
 
 const PlanEnd = ({ navigation }: Props) => {
   const setPlanAtom = useSetRecoilState(planState);
+  const createEditMode = useRecoilValue(editModeState);
   const [daySelected, setDaySelected] = useState(
     format(new Date(), 'yyyy-MM-dd')
   );
   const [timeSelected, setTimeSelected] = useState('');
 
   const reset = useResetRecoilState(planState);
+  const editReset = useResetRecoilState(editModeState);
 
   const isDisabled = daySelected === '' || timeSelected === '';
 
@@ -33,12 +36,28 @@ const PlanEnd = ({ navigation }: Props) => {
 
   // 일정 취소
   const handelCancel = () => {
-    reset();
+    createEditMode.mode ? editReset() : reset();
     setDaySelected('');
     setTimeSelected('');
 
     navigation.navigate('PlanCalendarScreen');
   };
+
+  // 수정 모드일시
+  useEffect(() => {
+    if (createEditMode.mode) {
+      const dateTime = new Date(createEditMode.detail.endDateTime);
+
+      // 날짜 부분 추출 (YYYY-MM-DD 형식)
+      const datePart = dateTime.toISOString().split('T')[0];
+      setDaySelected(datePart);
+
+      // 시간 부분 추출 (HH:mm 형식)
+      const timePart = dateTime.toTimeString().slice(0, 5);
+      setTimeSelected(timePart);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [createEditMode.mode]);
 
   return (
     <View style={{ flex: 1 }}>
