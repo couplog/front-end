30b3ad435f46b999e4b43bdfc191f0cfd6b26b70
hyeone999 <caps026@naver.com>
2023-08-02 16:41:46 +0900

    feat: 약관 페이지 uri 생성

---
+++ frontend/src/utils/uri.ts
@@ -0,0 +1,5 @@
+export const agree =
+  'https://useful-jonquil-43d.notion.site/3905bf9fd6ba407787038f87c31cd87c';
+
+export const service =
+  'https://useful-jonquil-43d.notion.site/0d38dddd9f4b464a9729740ae0e48b89';
