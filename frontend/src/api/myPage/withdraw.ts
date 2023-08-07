84524c271e94cb826ef63a2880acc467593d6ba3
hyeone999 <caps026@naver.com>
2023-08-07 16:16:18 +0900

    fix: 회원탈퇴 endpoint 수정

--- frontend/src/api/myPage/withdraw.ts
+++ frontend/src/api/myPage/withdraw.ts
@@ -3,6 +3,6 @@ import request from '../base';
 export const userWithdraw = (memberId: number | null) => {
   return request({
     method: 'DELETE',
-    url: `/api/members/${memberId}/withdrawal`,
+    url: `/api/members/${memberId}`,
   });
 };
